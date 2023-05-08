292c510842f9d3e376d048820c0189eeaa21c998
faysalahmmed1 <faysalahmmedcse@gmail.com>
2023-05-08 17:56:00 +0600

    my third commit

--- src/Components/Summary/Summary.js
+++ src/Components/Summary/Summary.js
@@ -2,14 +2,23 @@ import React from 'react';
 import './Summary.css'
 
 const Summary = ({cart}) => {
+    console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tex =parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tex;
     return (
         <div className='Order-Summary'>
-            <h1>Order Summary</h1>
+            <h1>Customer Order Summary</h1>
             <p className='Selected-Summary'>Selected summary: $ {cart.length}</p>
-            <p>Total Price: $ </p>
-            <p>Total Shopping Charge: $</p>
-            <p>Tex: %</p>
-            <h3>Grand Total: $ </h3>
+            <p>Total Price: ${total} </p>
+            <p>Total Shopping Charge: $ {shipping}</p>
+            <p>Tex: ${tex} </p>
+            <h3>Grand Total: ${grandTotal} </h3>
 
             <button className='Clear-Cart'>Clear Cart </button> <br />
             <button className='Review-Order'>Review Order</button>
